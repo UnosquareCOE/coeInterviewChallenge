f06fdbc4a6f3b2a8fd96c48410a1dbc33229672b
Diarmuid Doran <ddoran08@qub.ac.uk>
2022-04-24 18:57:15 +0100

    method structure added to the Circle, Rectangle and Square classes. Within the Algorithims the reverse string method was added and its associated test uncommented.

--- src/main/java/shapes/Circle.java
+++ src/main/java/shapes/Circle.java
@@ -1,5 +1,60 @@
 package shapes;
 
 public class Circle extends Shape {
-  // Add Code Here
+
+	// Add Code Here
+	// instance vars
+	private double radius;
+
+	// default constructor
+	public Circle() {
+
+	}
+
+	// constructor with args
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+
+	// methods
+	/**
+	 * @return radius
+	 */
+	public double getRadius() {
+		return radius;
+	}
+
+	/**
+	 * sets the double to radius value input
+	 * 
+	 * @param radius
+	 */
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	/**
+	 * Calculates the area of the circle
+	 * 
+	 * @return - area
+	 */
+	@Override
+	public double calculateArea() {
+		double area;
+		area =   Math.PI * Math.pow(radius, 2);
+		return area;
+	}
+
+	/**
+	 * Calculates the perimeter of the circle
+	 * 
+	 * @return - perimeter
+	 */
+	@Override
+	public double calculatePerimeter() {
+		double perimeter;
+		perimeter = radius * 2 * Math.PI;
+		return perimeter;
+	}
+
 }
--- src/main/java/shapes/Square.java
+++ src/main/java/shapes/Square.java
@@ -1,5 +1,56 @@
 package shapes;
 
 public class Square extends Shape {
-  // Add Code Here
+	// Add Code Here
+	// instance vars
+	private double side;
+	
+	// default constructor
+	public Square() {
+
+	}
+
+	// constructor with args
+	public Square(double side) {
+		this.side = side;
+	}
+
+	/**
+	 * @return - side
+	 */
+	public double getSide() {
+		return side;
+	}
+
+	/**
+	 * set the length of the square sides
+	 * 
+	 * @param side
+	 */
+	public void setSide(double side) {
+		this.side = side;
+	}
+
+	/**
+	 * Calculates the area of the square
+	 * 
+	 * @return - area
+	 */
+	@Override
+	public double calculateArea() {
+		double area = Math.pow(side, 2);
+		return area;
+	}
+
+	/**
+	 * Calculates the perimeter of the square
+	 * 
+	 * @return - perimeter
+	 */
+	@Override
+	public double calculatePerimeter() {
+		double perimeter = side*4;
+		return perimeter;
+	}
 }
+
