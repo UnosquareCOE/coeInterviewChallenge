fd63ce8dced91160516116c3e4c5adbf8005821a
Wren00 <lorenwalsh00@gmail.com>
2022-05-02 18:27:44 +0100

    Rectangle code updated

--- src/main/java/shapes/Rectangle.java
+++ src/main/java/shapes/Rectangle.java
@@ -2,4 +2,23 @@ package shapes;
 
 public class Rectangle extends Shape {
   // Add Code Here
+
+  private double width;
+  private double height;
+
+  public Rectangle(double width, double height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  public double calculateArea() {
+    double area = width * height;
+    return area;
+  }
+
+  public double calculatePerimeter()  {
+    double perimeter = 2 * (height + width);
+    return perimeter;
+  }
+
 }
