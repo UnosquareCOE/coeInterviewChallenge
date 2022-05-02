0dee5d1c016841a3ae77105676d07d77956c750b
nmcgowan99 <97925088+nmcgowan99@users.noreply.github.com>
2022-05-02 13:07:24 +0100

    Code updated
    Code added to Algorithms, Square, Circle and Rectangles classes. All tests ran successfully.

--- src/main/java/algorithms/Algorithms.java
+++ src/main/java/algorithms/Algorithms.java
@@ -1,26 +1,40 @@
 package algorithms;
 
+//import java.util.List;
 
-import java.util.List;
-
+/**
+ * Algorithms class
+ * 
+ */
 public class Algorithms {
 
-  // README:
-  // Uncomment one of the methods below and add an implementation
+	// README:
+	// Uncomment one of the methods below and add an implementation
 
-//  static int countOddNumbersInCollection(final List<Integer> integerList)
-//  {
-//
-//  }
+	// static int countOddNumbersInCollection(final List<Integer> integerList)
+	// {
+	//
+	// }
 
-//  static int calculateSumOfEvenNumbersInCollection(final List<Integer> integerList)
-//  {
-//
-//  }
+	// static int calculateSumOfEvenNumbersInCollection(final List<Integer>
+	// integerList)
+	// {
+	//
+	// }
 
-//  static String reverseString(final String stringToReverse)
-//  {
-//
-//  }
+	/**
+	 * Reverses a string
+	 * 
+	 * @param stringToReverse
+	 * @return
+	 */
+	static String reverseString(final String stringToReverse) {
+
+		if (stringToReverse.length() == 1) {
+			// base case
+			return stringToReverse;
+		}
+		return reverseString(stringToReverse.substring(1)) + stringToReverse.charAt(0);
+	}
 
 }
--- src/main/java/shapes/Square.java
+++ src/main/java/shapes/Square.java
@@ -1,5 +1,55 @@
 package shapes;
 
+/**
+ * Square class
+ * 
+ * @author Nicola McGowan
+ */
 public class Square extends Shape {
-  // Add Code Here
+
+	private static final double SIDE = 7;
+
+	private double side;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param side
+	 */
+	public Square(double side) {
+		super();
+		this.setSide(side);
+	}
+
+	/**
+	 * @return the side
+	 */
+	public double getSide() {
+		return side;
+	}
+
+	/**
+	 * @param side the side to set
+	 */
+	public void setSide(double side) {
+		this.side = side;
+	}
+
+	/**
+	 * Calculates area of a square
+	 */
+	@Override
+	public double calculateArea() {
+		final double area = SIDE * SIDE;
+		return area;
+	}
+
+	/**
+	 * Calculates perimeter of a square
+	 */
+	@Override
+	public double calculatePerimeter() {
+		final double perimeter = SIDE * 4;
+		return perimeter;
+	}
 }
