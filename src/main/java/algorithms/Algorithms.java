a25a72b348d4aee945b658e4019acb37d50a2ead
Eamon Ronan <75735467+eamonronan@users.noreply.github.com>
2022-05-03 14:51:32 +0100

    Eamon Ronan UnoSquare coding challenge

--- src/main/java/algorithms/Algorithms.java
+++ src/main/java/algorithms/Algorithms.java
@@ -1,26 +1,31 @@
 package algorithms;
-
-
 import java.util.List;
-
 public class Algorithms {
 
   // README:
   // Uncomment one of the methods below and add an implementation
 
-//  static int countOddNumbersInCollection(final List<Integer> integerList)
-//  {
-//
-//  }
+  // method to determine number of odd integers in a collection
+  static int countOddNumbersInCollection(final List<Integer> integerList) {
+    int oddCount;
+    oddCount = 0;
+    for (int i : integerList) {
+      if (i % 2 == 1) {
+        oddCount++;
+      }
+    }
+    return oddCount;
+  }
 
-//  static int calculateSumOfEvenNumbersInCollection(final List<Integer> integerList)
-//  {
-//
-//  }
+  // static int calculateSumOfEvenNumbersInCollection(final List<Integer>
+  // integerList)
+  // {
+  //
+  // }
 
-//  static String reverseString(final String stringToReverse)
-//  {
-//
-//  }
+  // static String reverseString(final String stringToReverse)
+  // {
+  //
+  // }
 
 }
--- src/main/java/shapes/Circle.java
+++ src/main/java/shapes/Circle.java
@@ -1,5 +1,31 @@
 package shapes;
 
 public class Circle extends Shape {
-  // Add Code Here
+
+  private double radius;
+
+  public Circle(double radius) {
+    setRadius(radius);
+  }
+
+  public void setRadius(double radius) {
+    if (radius <= 0) {
+      throw new IllegalArgumentException("Radius must be greater than 0!");
+    } else {
+      this.radius = radius;
+    }
+  }
+
+  public double getRadius() {
+    return this.radius;
+  }
+
+  public double calculateArea() {
+    return radius * radius * Math.PI;
+  }
+
+  public double calculatePerimeter() {
+    return 2 * radius * Math.PI;
+  }
+
 }
