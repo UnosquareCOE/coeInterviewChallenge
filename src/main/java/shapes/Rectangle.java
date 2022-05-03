804879ccefd70edadfed9cd3d5c91185dc65afcc
Eamon Ronan <75735467+eamonronan@users.noreply.github.com>
2022-05-03 15:31:02 +0100

    Updated code for Square, Circle, Rectangle, Algorithms, and AlgorithmsTest classes

--- src/main/java/shapes/Rectangle.java
+++ src/main/java/shapes/Rectangle.java
@@ -1,5 +1,46 @@
 package shapes;
 
 public class Rectangle extends Shape {
-  // Add Code Here
+
+  private double length;
+  private double width;
+
+  public Rectangle(double length, double width) {
+    setLength(length);
+    setWidth(width);
+
+  }
+
+  public double getLength() {
+    return this.length;
+  }
+
+  public double getWidth() {
+    return this.width;
+  }
+
+  public void setLength(double length) {
+    if (length <= 0) {
+      throw new IllegalArgumentException("Rectangle length must be greater than 0!");
+    } else {
+      this.length = length;
+    }
+  }
+
+  public void setWidth(double width) {
+    if (width <= 0) {
+      throw new IllegalArgumentException("Rectangle width must be greater than 0!");
+    } else {
+      this.width = width;
+    }
+  }
+
+  public double calculateArea() {
+    return length * width;
+  }
+
+  public double calculatePerimeter() {
+    return (2 * length) + (2 * width);
+  }
+
 }
